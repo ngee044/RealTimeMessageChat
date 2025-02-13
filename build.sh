9802ff7c2e1e89bb11448412b2c6fa9b7318d38f
ngee044 <38107616+ngee044@users.noreply.github.com>
2025-02-13 21:48:40 +0900

    u: 오타 수정

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ rm -rf build
 
 mkdir build
 cd build
-cmake .. -DCAMKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCAMKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCAMKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
